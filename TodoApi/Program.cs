6693d862351a4b7768ba45f3ed2e55ae09880e29
Ki-Joon PARK <glycerine@live.co.kr>
2023-05-24 17:36:59 +0900

    add: 자습서: ASP.NET Core를 사용하여 웹 API 만들기 (working) - https://learn.microsoft.com/ko-kr/aspnet/core/tutorials/first-web-api?view=aspnetcore-7.0&tabs=visual-studio

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,12 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+// DI 컨테이너에 데이터베이스 컨텍스트를 추가 및 데이터베이스 컨텍스트가 메모리 내 데이터베이스를 사용하도록 지정
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;
+});
 
 var app = builder.Build();
 
